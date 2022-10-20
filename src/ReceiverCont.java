864b04a55e8820b556ed9f2f588a350c89541ed3
adaaaaaaaaa <44308146+adaaaaaaaaa@users.noreply.github.com>
2022-10-20 15:51:40 +0800

    Add files via upload

---
+++ src/ReceiverCont.java
@@ -0,0 +1,9 @@
+public class ReceiverCont {
+    public void add(){
+        System.out.println("add");
+    }
+    public void delete(){
+        System.out.println("delete...");
+    }
+}
+
---
+++ src/ReceiverStruc.java
@@ -0,0 +1,8 @@
+public class ReceiverStruc {
+    public void load(){
+        System.out.println("load...");
+    }
+    public void showTree(){
+        System.out.println("showTree...");
+    }
+}
