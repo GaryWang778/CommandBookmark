b92dc9b7a54ca2bce8f453ccd698ac7f1bd5e0fa
adaaaaaaaaa <44308146+adaaaaaaaaa@users.noreply.github.com>
2022-10-23 21:52:33 +0800

    Delete src directory

--- src/ReceiverCont.java
+++
@@ -1,9 +0,0 @@
-public class ReceiverCont {
-    public void add(){
-        System.out.println("add");
-    }
-    public void delete(){
-        System.out.println("delete...");
-    }
-}
-
--- src/ReceiverStruc.java
+++
@@ -1,8 +0,0 @@
-public class ReceiverStruc {
-    public void load(){
-        System.out.println("load...");
-    }
-    public void showTree(){
-        System.out.println("showTree...");
-    }
-}
