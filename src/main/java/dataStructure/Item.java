a18dc5774f241d24a10bfee2911354407dea7dc5
FlameRain926 <73727228+FlameRain926@users.noreply.github.com>
2022-10-29 23:02:06 +0800

    Update Item.java

--- src/main/java/dataStructure/Item.java
+++ src/main/java/dataStructure/Item.java
@@ -7,13 +7,11 @@ public class Item {
     public Item(){
         this.sons = new ArrayList<Item>();
         this.title = new String;
-        this.level = new int;
         this.bookmark = new String;
         this.hyperlink = new String;
         this.visited = new int;
     }
     public String title;//书签的标题
-    public int level;//书签的级别
     public String bookmark;//书签的描述，例如elearning Markdown Guide
     public String hyperlink;//书签的链接
     public ArrayList<Item> sons;//子书签
