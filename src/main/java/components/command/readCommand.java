a78448bfc3cc29711193642d28388710244bb5cc
FlameRain926 <73727228+FlameRain926@users.noreply.github.com>
2022-10-29 23:24:09 +0800

    Update readCommand.java

--- src/main/java/components/command/readCommand.java
+++ src/main/java/components/command/readCommand.java
@@ -3,26 +3,36 @@ import dataStructure.Item;
 import dataStructure.Label;
 
 //实现read-bookMark功能
-public class readbookMark implements Command{
+public class readCommand implements Command{
+    public String bookmarkName = null;
+    public String cmdName;
+    
+    public readCommand(String[] list){
+        cmdName = list[0];
+        if(cmdName.equals("read-bookMark "))
+            bookMarkName = list[1];
+    }
+    
     @Override
-    public void execute(Label bookMark, String url) {//url是待读取的bookmark
+    public void execute(Label bookMark) {
         ArrayList<Item> temp = bookMark.items;
+        String tempstring = this.bookmarkName;
         for(Item i : temp){
-            read_mark(i, url);
+            read_mark(i, tempstring);
         }
         System.out.println("readCommand is executed");
     }
 
-    public static void read_mark(Item item,String url){
+    public static void read_mark(Item item,String name){//name是待读取的bookmarkName
         if(item == null)
             return;
-        if(item.bookmark == url){
+        if(item.bookmark == name){
             item.visited++;
             return;}
         ArrayList<Item> temp1 = item.sons;
         if (temp1 != null)
             for(Item i : temp1){
-                read_mark(i,url);
+                read_mark(i,name);
             }
     }
     
