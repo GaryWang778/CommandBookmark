c26426a7abdc8b9f9346edab36a5d56ea015d596
FlameRain926 <73727228+FlameRain926@users.noreply.github.com>
2022-10-29 23:02:48 +0800

    Update Label.java

--- src/main/java/dataStructure/Label.java
+++ src/main/java/dataStructure/Label.java
@@ -5,10 +5,8 @@ import java.util.ArrayList;
 public class Label {
     public Label() {//构造函数
         this.items = new ArrayList<Item>();
-        this.all_items = new ArrayList<Item>();
     }
 
-    public ArrayList<Item> items;//一级书签，方便用于showtree
-    public ArrayList<Item> all_items;//所有书签，方便add与delete操作
+    public ArrayList<Item> items;//当前Label下的所有一级书签
 
 }
