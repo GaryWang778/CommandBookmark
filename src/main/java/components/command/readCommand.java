9abd15e4e0f69647bc9c36df5690cf74fa00634e
FlameRain926 <73727228+FlameRain926@users.noreply.github.com>
2022-10-29 23:04:28 +0800

    add readCommand

--- src/main/java/components/command/readCommand.java
+++ src/main/java/components/command/readCommand.java
@@ -1,23 +1,29 @@
 package components.command;
-
+import dataStructure.Item;
 import dataStructure.Label;
 
-public class readCommand implements Command {
-    public String cmdName;
-    public String boookmarkName;
-
-    public readCommand(String[] list){
-        cmdName = list[0];
-        boookmarkName = list[1];
-    }
+//实现read-bookMark功能
+public class readbookMark implements Command{
     @Override
-    public void execute(Label bookMark) {
-        //执行read-bookmark指令
-        System.out.print(cmdName+"\n"+boookmarkName+"\n");
+    public void execute(Label bookMark, String url) {//url是待读取的bookmark
+        ArrayList<Item> temp = bookMark.items;
+        for(Item i : temp){
+            read_mark(i, url);
+        }
+        System.out.println("readCommand is executed");
+    }
 
+    public static void read_mark(Item item,String url){
+        if(item == null)
+            return;
+        if(item.bookmark == url){
+            item.visited++;
+            return;}
+        ArrayList<Item> temp1 = item.sons;
+        if (temp1 != null)
+            for(Item i : temp1){
+                read_mark(i,url);
+            }
     }
-    @Override
-    public void undo(Label bookMark) {
-        System.out.println("readCommand is canceled");
-    }
+    
 }
