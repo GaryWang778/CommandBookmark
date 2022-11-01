f4408dd1aba13e00b55d463ead825e51f5a14638
adaaaaaaaaa <44308146+adaaaaaaaaa@users.noreply.github.com>
2022-11-01 21:00:22 +0800

    Add files via upload

---
+++ commandManagerTest.java
@@ -0,0 +1,39 @@
+package components.command;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class commandManagerTest {
+    private String[] list = {""};
+    addCommand addCommand = new addCommand(list);
+    deleteCommand deleteCommand = new deleteCommand(list);
+
+    @Test
+    void undo() {
+        List<Command> expectedCommand = new ArrayList<>();
+        commandManager.executeCommand(addCommand);
+        expectedCommand.add(addCommand);
+        Assertions.assertIterableEquals(expectedCommand,commandManager.getHistoryCommands(),"命令插入失败");
+
+        commandManager.undo();
+        expectedCommand.remove(addCommand);
+        Assertions.assertIterableEquals(expectedCommand,commandManager.getHistoryCommands(),"undo失败");
+    }
+
+    @Test
+    void redo() {
+        List<Command> expectedCommand = new ArrayList<>();
+        commandManager.executeCommand(addCommand);
+        expectedCommand.add(addCommand);
+        Assertions.assertIterableEquals(expectedCommand,commandManager.getHistoryCommands(),"命令插入失败");
+
+        commandManager.undo();
+        commandManager.redo();
+        Assertions.assertIterableEquals(expectedCommand,commandManager.getHistoryCommands(),"redo失败");
+    }
+}
